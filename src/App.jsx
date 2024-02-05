cd83296ef283e8fa9fe2a629647c45a730837ea5
Renan Pessanha <142458180+renanpessanh@users.noreply.github.com>
2024-02-05 09:18:31 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home";
 
